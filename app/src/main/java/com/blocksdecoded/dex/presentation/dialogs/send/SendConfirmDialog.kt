a88fc4743c7288d96e76e0ecde8250ae3c626211
Askar <myrsabekov@gmail.com>
2019-07-31 15:46:52 +0600

    Update balance item design

---
+++ app/src/main/java/com/blocksdecoded/dex/presentation/dialogs/send/SendConfirmDialog.kt
@@ -0,0 +1,28 @@
+package com.blocksdecoded.dex.presentation.dialogs.send
+
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import com.blocksdecoded.dex.core.model.Coin
+import java.math.BigDecimal
+
+class SendConfirmDialog: DialogFragment() {
+
+    companion object {
+        fun show(fragmentManager: FragmentManager, data: SendConfirmData) {
+
+        }
+    }
+
+    data class SendConfirmData(
+        val coin: Coin,
+        val address: String,
+        val amount: BigDecimal,
+        val fee: BigDecimal
+    )
+
+    interface SendConfirmListener {
+        fun onConfirm()
+
+        fun onDismiss()
+    }
+}
