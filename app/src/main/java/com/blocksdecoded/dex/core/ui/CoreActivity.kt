352fd00d0555dc354ae3f4a4ec2ee8245d5b5af2
Askar <myrsabekov@gmail.com>
2019-09-04 16:27:52 +0600

    Guest onboarding logic

--- app/src/main/java/com/blocksdecoded/dex/core/ui/CoreActivity.kt
+++ app/src/main/java/com/blocksdecoded/dex/core/ui/CoreActivity.kt
@@ -2,9 +2,12 @@ package com.blocksdecoded.dex.core.ui
 
 import android.graphics.Color
 import android.view.Gravity
+import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.annotation.ColorInt
 import androidx.appcompat.app.AppCompatActivity
 import com.blocksdecoded.dex.App
 
@@ -16,7 +19,41 @@ abstract class CoreActivity: AppCompatActivity() {
         }
     }
 
-    private fun addTestLabel() {
+    //region Status bar
+
+    protected fun initTransparentStatusBar() {
+        val contentParent = findViewById<ViewGroup>(android.R.id.content)
+        val content = contentParent.getChildAt(0)
+        setFitsSystemWindows(content, fitSystemWindows = false, applyToChildren = true)
+    }
+
+    protected fun setStatusBarImmersiveMode(@ColorInt color: Int) {
+        val win = window
+        win.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        win.attributes.systemUiVisibility = win.attributes.systemUiVisibility or (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        win.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        win.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        win.statusBarColor = color
+    }
+
+    private fun setFitsSystemWindows(view: View?, fitSystemWindows: Boolean, applyToChildren: Boolean) {
+        if (view == null) return
+        view.fitsSystemWindows = fitSystemWindows
+        if (applyToChildren && view is ViewGroup) {
+            var i = 0
+            view.let {
+                val n = view.childCount
+                while (i < n) {
+                    view.getChildAt(i).fitsSystemWindows = fitSystemWindows
+                    i ++
+                }
+            }
+        }
+    }
+
+    //endregion
+
+    open fun addTestLabel() {
         val rootView = findViewById<ViewGroup>(android.R.id.content)
         val testLabelTv = TextView(this)
         testLabelTv.text = "Ropsten Testnet"
--- app/src/main/java/com/blocksdecoded/dex/presentation/guest/GuestActivity.kt
+++ app/src/main/java/com/blocksdecoded/dex/presentation/guest/GuestActivity.kt
@@ -3,22 +3,39 @@ package com.blocksdecoded.dex.presentation.guest
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.blocksdecoded.dex.R
 import com.blocksdecoded.dex.core.ui.CoreActivity
+import com.blocksdecoded.dex.core.ui.CoreFragment
 import com.blocksdecoded.dex.presentation.main.MainActivity
 import com.blocksdecoded.dex.presentation.restore.RestoreWalletActivity
 import com.blocksdecoded.dex.presentation.widgets.click.setSingleClickListener
 import kotlinx.android.synthetic.main.activity_guest.*
+import kotlinx.android.synthetic.main.fragment_guest_onboarding.*
 
 class GuestActivity : CoreActivity() {
 
     private lateinit var viewModel: GuestViewModel
+
+    private val onboardingPages = listOf(
+        R.string.onboarding_about_app to R.drawable.ic_about,
+        R.string.onboarding_long_text to R.drawable.ic_about,
+        R.string.onboarding_about_app to R.drawable.ic_about,
+        R.string.onboarding_about_app to R.drawable.ic_about,
+        R.string.onboarding_about_app to R.drawable.ic_about
+    )
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guest)
+        initTransparentStatusBar()
+
         viewModel = ViewModelProviders.of(this).get(GuestViewModel::class.java)
         
         viewModel.openBackupEvent.observe(this, Observer {
@@ -35,12 +52,51 @@ class GuestActivity : CoreActivity() {
         
         guest_create_wallet.setSingleClickListener { viewModel.onCreateClick() }
         guest_restore_wallet.setSingleClickListener { viewModel.onRestoreClick() }
+
+        onboarding_viewpager.adapter = GuestOnboardingAdapter(supportFragmentManager, onboardingPages)
+        onboarding_indicator.setViewPager(onboarding_viewpager)
     }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        setStatusBarImmersiveMode(ContextCompat.getColor(this, R.color.transparent))
+    }
+
+    override fun addTestLabel() = Unit
 
     companion object {
         fun start(context: Context) {
             context.startActivity(Intent(context, GuestActivity::class.java))
         }
     }
+}
 
+class GuestOnboardingAdapter(
+    fragmentManager: FragmentManager,
+    private val pages: List<Pair<Int, Int>>
+): FragmentPagerAdapter(fragmentManager) {
+    override fun getItem(position: Int): Fragment =
+        GuestOnboardingFragment.newInstance(pages[position])
+
+    override fun getCount(): Int = pages.size
+}
+
+class GuestOnboardingFragment: CoreFragment(R.layout.fragment_guest_onboarding) {
+    private var config: Pair<Int, Int>? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        config?.let {
+            onboarding_title?.setText(it.first)
+            onboarding_icon?.setImageResource(it.second)
+        }
+    }
+
+    companion object {
+        fun newInstance(config: Pair<Int, Int>): Fragment {
+            return GuestOnboardingFragment().apply {
+                this.config = config
+            }
+        }
+    }
 }
