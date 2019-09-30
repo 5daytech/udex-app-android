5a2fa390c37440f58b4a54dd66c44a810581d08b
Askar <myrsabekov@gmail.com>
2019-09-30 19:03:07 +0600

    Update primary button and accent text and icon colors

--- app/src/main/java/com/blocksdecoded/dex/LaunchActivity.kt
+++ app/src/main/java/com/blocksdecoded/dex/LaunchActivity.kt
@@ -3,8 +3,8 @@ package com.blocksdecoded.dex
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.blocksdecoded.dex.presentation.main.MainActivity
-import com.blocksdecoded.dex.core.ui.CoreActivity
 import com.blocksdecoded.dex.presentation.dialogs.AlertDialogFragment
 import com.blocksdecoded.dex.presentation.guest.GuestActivity
 import com.blocksdecoded.dex.presentation.keystore.KeyStoreActivity
@@ -12,7 +12,7 @@ import com.blocksdecoded.dex.presentation.pin.PinActivity
 import com.blocksdecoded.dex.utils.Logger
 import com.blocksdecoded.dex.utils.ui.ToastHelper
 
-class LaunchActivity: CoreActivity() {
+class LaunchActivity: AppCompatActivity() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
