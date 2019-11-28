6dfb33cfed2db9c4df3e7002408671a42154f030
Askar <myrsabekov@gmail.com>
2019-11-28 16:29:13 +0600

    Observe exchange history sync subject on UI thread and handle errors

--- app/src/main/java/com/fridaytech/dex/presentation/exchangehistory/ExchangeHistoryViewModel.kt
+++ app/src/main/java/com/fridaytech/dex/presentation/exchangehistory/ExchangeHistoryViewModel.kt
@@ -5,7 +5,10 @@ import com.fridaytech.dex.App
 import com.fridaytech.dex.core.ui.CoreViewModel
 import com.fridaytech.dex.core.ui.SingleLiveEvent
 import com.fridaytech.dex.data.manager.history.ExchangeRecord
+import com.fridaytech.dex.utils.Logger
 import com.fridaytech.dex.utils.isValidIndex
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class ExchangeHistoryViewModel : CoreViewModel() {
     private val exchangeHistoryManager = App.exchangeHistoryManager
@@ -17,15 +20,17 @@ class ExchangeHistoryViewModel : CoreViewModel() {
 
     init {
         exchangeHistoryManager.syncSubject
-            .subscribe { refreshTrades() }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ refreshTrades() }, { Logger.e(it) })
             .let { disposables.add(it) }
 
         refreshTrades()
     }
 
     private fun refreshTrades() {
-        trades.postValue(exchangeHistoryManager.exchangeHistory)
-        emptyTradesVisible.postValue(exchangeHistoryManager.exchangeHistory.isEmpty())
+        trades.value = exchangeHistoryManager.exchangeHistory
+        emptyTradesVisible.value = exchangeHistoryManager.exchangeHistory.isEmpty()
     }
 
     fun onTransactionClick(position: Int) {
